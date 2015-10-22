07b139153aa530819d8c1d6aee1d6c13247c462f
balladares <pozodicas@gmail.com>
2015-10-22 19:00:07 -0200

    Update the grid system and include some CSS effects

--- dev/assets/js/main.js
+++ dev/assets/js/main.js
@@ -2,4 +2,12 @@ $(document).ready(function(){
 	new WOW().init();
 	// Parallax
 	$('#chamada').parallax("50%", 0.8);
+	// Typed
+	$(function(){
+      $(".titulo-pagina h2 .typed").typed({
+         strings: ["Sites GBrand"],
+         contentType: 'html',
+         typeSpeed: 10
+      });
+    });
 });
