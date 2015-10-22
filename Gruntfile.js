2d21b0e9e46c5c09408ac3b45fd100cc17c4f575
balladares <pozodicas@gmail.com>
2015-10-22 09:35:03 -0200

    Init project

---
+++ Gruntfile.js
@@ -0,0 +1,92 @@
+module.exports = function(grunt){
+	// Configurações do projeto
+	grunt.initConfig({
+
+		// Configurações dos plugins
+		less:{
+			development:{
+				options:{
+					paths:["dev/assets/css"]
+				},
+				files:{
+					"dev/assets/css/galeria.css":"dev/assets/less/galeria.less",
+					"dev/assets/css/topo.css":"dev/assets/less/topo.less",
+					"dev/assets/css/rodape.css":"dev/assets/less/rodape.less"
+				}
+			}
+		}, // Less
+		cssmin: {
+        	target: {
+            	files: [{
+            		expand: true,
+      				cwd: 'dev/assets/css',
+      				src: ['main.css'],
+      				dest: 'dev/assets/css/min',
+      				ext: '.min.css'
+    			}]
+  			}
+		}, // Css Minifier
+		uglify: {
+     		my_target: {
+      			files: {
+        			'dev/assets/js/min/main.min.js': ['dev/assets/js/main.js']
+      			}
+    		}
+  		}, // Uglify
+ 	 	concat: {
+    		options: {
+      			separator: ';'
+    		},
+    		dist: {
+      			src: ['dev/assets/css/topo.css', 'dev/assets/css/galeria.css', 'dev/assets/css/rodape.css'],
+      			dest: 'dev/assets/css/main.css'
+    		}
+  		}, // Concat
+  		watch: {
+  			scripts: {
+    			files: ['dev/assets/js/main.js','dev/assets/less/topo.less','dev/assets/less/galeria.less',
+    			'dev/assets/less/rodape.less'],
+    			tasks: ['less','concat','cssmin','uglify'],
+    		options: {
+      			spawn: false
+    			}
+  			}
+		}, // Watch
+		'ftp-deploy': {
+      		build: {
+        		auth: {
+          			host: 'ftp.gbrand.com.br',
+          			port: 21,
+          			authKey: 'lab'
+        		},
+        		src: 'dev/',
+        		dest: 'www/lab/teste'
+      		}
+    	}, //Dploy
+   compress : {
+      main : {
+        options : {
+          archive : "download/nomeProj.zip"
+        },
+      files : [
+          { expand: true, src : "**/*", cwd : "dev/" }
+      ]
+    }
+  } // Compress
+	});
+
+	// Declarando plugins
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-ftp-deploy');
+  grunt.loadNpmTasks('grunt-contrib-compress');
+
+	// Tarefas a serem executadas
+	grunt.registerTask('default',['less','concat','cssmin','uglify']);
+	grunt.registerTask('w',['watch']);
+	grunt.registerTask('ftp',['ftp-deploy']);
+  grunt.registerTask('zip',['compress']);
+};
---
+++ dev/assets/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	// Botão de teste
+	$("#teste").hover(function(){
+		alert("That's ok, go hard!!");
+	});
+
+	$("#teste").hasClass(".teste");
+});
