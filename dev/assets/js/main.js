7806ad23d6170b6cdaed12361f12ecf08a5aa30c
balladares <pozodicas@gmail.com>
2015-10-23 14:58:07 -0200

    changes were done to make an interface likes the index page

--- dev/assets/js/main.js
+++ dev/assets/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	new WOW().init();
 	// Parallax
-	$('#chamada').parallax("50%", 0.8);
+	$('#chamada').parallax("50%", 0.5, "100%");
 	// Typed
 	$(function(){
       $(".titulo-pagina h2 .typed").typed({
@@ -9,5 +9,18 @@ $(document).ready(function(){
          contentType: 'html',
          typeSpeed: 10
       });
+    });
+
+    $(".cd-more-info").mouseenter(function(){
+
+    	if ($(".fases").hasClass("pulsar")) {
+    		$(".fases").removeClass("pulsar");
+    	};
+
+    	var id = $(this).attr("data-content-fase");
+
+    	$("."+id).addClass("pulsar");
+    }).mouseleave(function(){
+    	$(".cd-single-step").removeClass("pulsar");
     });
 });
