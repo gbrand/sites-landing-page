c3d5f67c79b4dc140d317573491240bdc9dc4d5a
balladares <pozodicas@gmail.com>
2015-10-23 18:06:56 -0200

    Add email form modules.

---
+++ dev/modulos/enviar-contato.php
@@ -0,0 +1,74 @@
+<?php
+
+
+// variáveis que guardam os dados vindo do form 
+$nomePessoa=utf8_decode($_POST['nome']);
+$email=utf8_decode($_POST['email']);
+$mensagem=utf8_decode($_POST['mensagem']);
+$telefone=utf8_decode($_POST['telefone']);
+$Data=date('d/m/Y');
+$Hora= date("H:i:s");
+
+require_once("class.smtp.php");
+
+$destinatarios = 'agencia@gbrand.com.br';
+
+$nomeDestinatario = utf8_decode("Agência GBrand");
+
+$usuario = 'agencia@gbrand.com.br';
+
+$senha = 'gbr501';
+
+
+/*abaixo as veriaveis principais, que devem conter em seu formulario*/
+$nomeRemetente = $_POST['nomePessoa'];
+$assunto = utf8_decode("Novo Contato");
+$msg = "
+<b>Nome do cliente:</b> $nomePessoa<br>
+<b>Email:</b> $email<br>
+<b>Telefone:</b> $telefone<br>
+<b>Mensagem:</b><br> $mensagem<br><br>
+Enviado do site da GBrand em $Data as $Hora 
+
+";
+
+
+/*********************************** A PARTIR DAQUI NAO ALTERAR ************************************/
+
+include_once("class.phpmailer.php");
+
+$To = $destinatarios;
+$Subject = $assunto;
+$Message = $msg;
+
+$Host = 'smtp.'.substr(strstr($usuario, '@'), 1);
+$Username = $usuario;
+$Password = $senha;
+$Port = "587";
+
+$mail = new PHPMailer();
+$body = $Message;
+$mail->IsSMTP(); // telling the class to use SMTP
+$mail->Host = $Host; // SMTP server
+$mail->SMTPDebug = 0; // enables SMTP debug information (for testing)
+// 1 = errors and messages
+// 2 = messages only
+$mail->SMTPAuth = true; // enable SMTP authentication
+$mail->Port = $Port; // set the SMTP port for the service server
+$mail->Username = $Username; // account username
+$mail->Password = $Password; // account password
+
+$mail->SetFrom($usuario, $nomeDestinatario);
+$mail->Subject = $Subject;
+$mail->MsgHTML($body);
+$mail->AddAddress($To, "");
+
+
+if ($mail->Send()) {
+        echo "<p class='lead'>Contato enviado com sucesso!</p>";
+}else{
+        echo "<script>alert('Erro ao enviar e-mail".print($mail->ErrorInfo)."');</script>";
+}
+
+
+?>
